314071805ac94a891b7393d3b229cd637ce80d91
Fayshal Rana <imfayshalrana@gmail.com>
2024-11-16 01:39:18 +0600

    fix controler

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,21 @@
-<?php
+<?php 
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+
+class UiController extends Controller
 {
-    //
+    public function welcomePage() {
+        return view('welcome');
+    }
+
+    // Page Specific
+//     public function homeController() {
+//         return view('ui.home');
+//     }
+//     public function aboutController() {
+//         return view('ui.about');
+// }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UiController;
+use Illuminate\Support\Facades\Http;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [UiController::class, 'welcomePage'])->name('welcome-page');
+
+Route::group(['prefix' => 'ui'], function () {
+    // 
+    Route::get('/home', [UiController::class, 'homeController'])->name('home');
+    Route::get('/about', [UiController::class, 'aboutController'])->name('about');
 });
