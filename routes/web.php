9b61547aba699e7413654c4a55d572cf13a1373c
Fayshal Rana <imfayshalrana@gmail.com>
2024-11-16 01:50:18 +0600

    fix route

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
 
-Route::get('/', [Controller::class, 'welcomePage'])->name('welcome-page');
+Route::get('/', [Controller::class, 'welcomePage'])->name('welcome');
 
-Route::group(['prefix' => 'ui'], function () {
-    // 
-    // Route::get('/home', [UiController::class, 'homeController'])->name('home');
-    // Route::get('/about', [UiController::class, 'aboutController'])->name('about');
-});
+// Route::group(['prefix' => 'ui'], function () {
+//     // 
+//     // Route::get('/home', [UiController::class, 'homeController'])->name('home');
+//     // Route::get('/about', [UiController::class, 'aboutController'])->name('about');
+// });
