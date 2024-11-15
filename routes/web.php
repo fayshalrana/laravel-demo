45046b734eb3efe904782be8e784681a95c96431
Fayshal Rana <imfayshalrana@gmail.com>
2024-11-16 01:40:00 +0600

    comment router

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ Route::get('/', [UiController::class, 'welcomePage'])->name('welcome-page');
 
 Route::group(['prefix' => 'ui'], function () {
     // 
-    Route::get('/home', [UiController::class, 'homeController'])->name('home');
-    Route::get('/about', [UiController::class, 'aboutController'])->name('about');
+    // Route::get('/home', [UiController::class, 'homeController'])->name('home');
+    // Route::get('/about', [UiController::class, 'aboutController'])->name('about');
 });
